a2f5040cfbfd72d04b3f6b0ec68d2e72df5b3d3e
7Levy <15958732798@163.com>
2022-11-11 13:57:28 +0800

    update

---
+++ get-namehash.js
@@ -0,0 +1,8 @@
+const namehash = require("eth-ens-namehash");
+let hash = namehash.hash("s💝s.eth");
+let input = "s💝s.eth";
+console.log(hash);
+// '0xde9b09fd7c5f901e23a3f19fecc54828e9c848539801e86591bd9801b019f84f'
+
+// Also supports normalizing strings to ENS compatibility:
+console.log(namehash.normalize(input));
